4f87b5122828be8419c2e7aa442013bbe366d455
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2014-10-18 14:52:53 -0700

    Turn on some examples

--- examples/A.hs
+++ examples/A.hs
@@ -9,15 +9,13 @@ module A where
 import GHC.TypeLits
 import Data.Proxy
 
-{-
 f :: ((a + 6) ~ x) => Proxy x -> ()
 f = g
 
 g :: ((6 <=? x) ~ True) => Proxy x -> ()
 g _ = ()
--}
 
-{-
+--{-
 data Vec :: Nat -> * -> * where
   Nil :: Vec 0 a
   Cons :: a -> Vec n a -> Vec (n + 1) a
@@ -32,7 +30,7 @@ reverse = go Nil
   go :: Vec m a -> Vec n a -> Vec (m + n) a
   go xs Nil = xs
   go xs (Cons y ys) = go (Cons y xs) ys
--}
+--}
 
 {-
 f :: Proxy (2 + a) -> ()
