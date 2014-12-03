1cb5daf9cb1db38d3c83dd423bb968cac7940487
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2014-12-02 19:29:27 -0800

    Filter out things appearing in wanted or derived.

--- src/TypeNatSolver.hs
+++ src/TypeNatSolver.hs
@@ -133,7 +133,7 @@ solverEntry s givens derived wanteds =
 
        TcPluginOk [] new_cts ->
           do (solved,_) <- solverSimplify s wanteds
-             let known = map ctPred derived
+             let known = map ctPred (derived ++ wanteds)
              return (TcPluginOk solved (filter (ctNotMember known) new_cts))
 
        TcPluginOk _ _ -> panic "solveImprove returned Solved!"
